0c7e32889903b192b9ebac08c56a45eecb6418df
Povilas Balciunas <balciunas90@gmail.com>
2014-07-27 15:07:02 +0300

    Removes duplicate html_attribute_struct declaration.

--- src/pugihtml.cpp
+++ src/pugihtml.cpp
@@ -180,25 +180,6 @@ namespace
 }
 #endif
 
-namespace pugihtml
-{
-	/// A 'name=value' HTML attribute structure.
-	struct html_attribute_struct
-	{
-		/// Default ctor
-		html_attribute_struct(html_memory_page* page): header(reinterpret_cast<uintptr_t>(page)), name(0), value(0), prev_attribute_c(0), next_attribute(0)
-		{
-		}
-
-		uintptr_t header;
-
-		char_t* name;	///< Pointer to attribute name.
-		char_t*	value;	///< Pointer to attribute value.
-
-		html_attribute_struct* prev_attribute_c;	///< Previous attribute (cyclic list)
-		html_attribute_struct* next_attribute;	///< Next attribute
-	};
-}
 
 namespace
 {
