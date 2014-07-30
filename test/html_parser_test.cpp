28478373c1434452817369d5ab325271b951df52
Povilas Balciunas <balciunas90@gmail.com>
2014-07-30 15:41:45 +0300

    Adds html void element parsing tests.

--- test/html_parser_test.cpp
+++ test/html_parser_test.cpp
@@ -29,3 +29,45 @@ TEST(html_parser, parse)
 		.first_child().value();
 	ASSERT_EQ("Hello world", parsed_str);
 }
+
+
+TEST(html_parser, parse_with_void_element_self_closing)
+{
+	char str_html[] =
+		"<html>"
+			"<body>"
+				"<br/>"
+				"<p>Hello world</p>"
+			"</body>"
+		"</hmtl>"
+	;
+
+	html_document doc;
+	html_parse_result res = html_parser::parse(str_html,
+		sizeof(str_html), doc.internal_object());
+
+	string parsed_str = doc.child("HTML").child("BODY").child("P")
+		.first_child().value();
+	ASSERT_EQ("Hello world", parsed_str);
+}
+
+
+TEST(html_parser, parse_with_void_element)
+{
+	char str_html[] =
+		"<html>"
+			"<body>"
+				"<br>"
+				"<p>Hello world</p>"
+			"</body>"
+		"</hmtl>"
+	;
+
+	html_document doc;
+	html_parse_result res = html_parser::parse(str_html,
+		sizeof(str_html), doc.internal_object());
+
+	string parsed_str = doc.child("HTML").child("BODY").child("P")
+		.first_child().value();
+	ASSERT_EQ("Hello world", parsed_str);
+}
