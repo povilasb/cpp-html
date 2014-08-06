7e9d3bfe159c85fe072f4049a7371067e86951b8
Povilas Balciunas <balciunas90@gmail.com>
2014-08-06 21:53:39 +0300

    Renames html attribute header file.

--- src/attribute.hpp
+++ src/attribute.hpp
@@ -1,5 +1,5 @@
-#ifndef PUGIHTML_HTML_ATTRINBUTE_HPP
-#define PUGIHTML_HTML_ATTRINBUTE_HPP 1
+#ifndef PUGIHTML_ATTRIBUTE_HPP
+#define PUGIHTML_ATTRIBUTE_HPP 1
 
 #include "memory.hpp"
 #include "common.hpp"
@@ -11,8 +11,8 @@ namespace pugihtml
 /**
  * A 'name=value' HTML attribute structure.
  */
-struct html_attribute_struct {
-	html_attribute_struct(html_memory_page* page)
+struct attribute_struct {
+	attribute_struct(html_memory_page* page)
 		: header(reinterpret_cast<uintptr_t>(page)), name(0), value(0),
 		prev_attribute_c(0), next_attribute(0)
 	{
@@ -24,32 +24,32 @@ struct html_attribute_struct {
 	char_t* value; // Pointer to attribute value.
 
 	// Previous attribute (cyclic list)
-	html_attribute_struct* prev_attribute_c;
-	html_attribute_struct* next_attribute; // Next attribute
+	attribute_struct* prev_attribute_c;
+	attribute_struct* next_attribute; // Next attribute
 };
 
 
 class html_node;
-class html_attribute_iterator;
+class attribute_iterator;
 
 /**
  * A light-weight handle for manipulating attributes in DOM tree.
  */
-class PUGIHTML_CLASS html_attribute {
-	friend class html_attribute_iterator;
+class PUGIHTML_CLASS attribute {
+	friend class attribute_iterator;
 	friend class html_node;
 
 private:
-	html_attribute_struct* _attr;
+	attribute_struct* _attr;
 
-	typedef html_attribute_struct* html_attribute::*unspecified_bool_type;
+	typedef attribute_struct* attribute::*unspecified_bool_type;
 
 public:
 	// Default constructor. Constructs an empty attribute.
-	html_attribute();
+	attribute();
 
 	// Constructs attribute from internal pointer
-	explicit html_attribute(html_attribute_struct* attr);
+	explicit attribute(attribute_struct* attr);
 
 	// Safe bool conversion operator
 	operator unspecified_bool_type() const;
@@ -58,12 +58,12 @@ public:
 	bool operator!() const;
 
 	// Comparison operators (compares wrapped attribute pointers)
-	bool operator==(const html_attribute& r) const;
-	bool operator!=(const html_attribute& r) const;
-	bool operator<(const html_attribute& r) const;
-	bool operator>(const html_attribute& r) const;
-	bool operator<=(const html_attribute& r) const;
-	bool operator>=(const html_attribute& r) const;
+	bool operator==(const attribute& r) const;
+	bool operator!=(const attribute& r) const;
+	bool operator<(const attribute& r) const;
+	bool operator>(const attribute& r) const;
+	bool operator<=(const attribute& r) const;
+	bool operator>=(const attribute& r) const;
 
 	// Check if attribute is empty
 	bool empty() const;
@@ -92,30 +92,30 @@ public:
 	bool set_value(bool rhs);
 
 	// Set attribute value (equivalent to set_value without error checking)
-	html_attribute& operator=(const char_t* rhs);
-	html_attribute& operator=(int rhs);
-	html_attribute& operator=(unsigned int rhs);
-	html_attribute& operator=(double rhs);
-	html_attribute& operator=(bool rhs);
+	attribute& operator=(const char_t* rhs);
+	attribute& operator=(int rhs);
+	attribute& operator=(unsigned int rhs);
+	attribute& operator=(double rhs);
+	attribute& operator=(bool rhs);
 
 	// Get next/previous attribute in the attribute list of the parent node
-	html_attribute next_attribute() const;
-	html_attribute previous_attribute() const;
+	attribute next_attribute() const;
+	attribute previous_attribute() const;
 
 	// Get hash value (unique for handles to the same object)
 	size_t hash_value() const;
 
 	// Get internal pointer
-	html_attribute_struct* internal_object() const;
+	attribute_struct* internal_object() const;
 };
 
 #ifdef __BORLANDC__
 	// Borland C++ workaround
-	bool PUGIHTML_FUNCTION operator&&(const html_attribute& lhs, bool rhs);
-	bool PUGIHTML_FUNCTION operator||(const html_attribute& lhs, bool rhs);
+	bool PUGIHTML_FUNCTION operator&&(const attribute& lhs, bool rhs);
+	bool PUGIHTML_FUNCTION operator||(const attribute& lhs, bool rhs);
 #endif
 
 } // pugihtml.
 
 
-#endif /* PUGIHTML_HTML_ATTRINBUTE_HPP */
+#endif /* PUGIHTML_ATTRIBUTE_HPP */
