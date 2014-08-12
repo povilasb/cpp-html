d56bc2fc6e11fc4c1952d96642ef26bd80c1c835
Povilas Balciunas <balciunas90@gmail.com>
2014-08-12 20:10:13 +0300

    Adds document::links() tests.

--- test/document_test.cpp
+++ test/document_test.cpp
@@ -27,3 +27,30 @@ TEST(document, get_element_by_id)
 	ASSERT_NE(nullptr, child);
 	ASSERT_EQ("div", child->name());
 }
+
+
+TEST(document, links)
+{
+	auto doc = html::document::create();
+
+	auto div = html::node::create(html::node_element);
+	div->name("div");
+	doc->append_child(div);
+
+	auto anchor = html::node::create(html::node_element);
+	anchor->name("A");
+	div->append_child(anchor);
+
+	auto area = html::node::create(html::node_element);
+	area->name("AREA");
+	div->append_child(area);
+
+	auto links = doc->links();
+	ASSERT_EQ(2u, links.size());
+
+	ASSERT_NE(nullptr, links[0]);
+	ASSERT_EQ("A", links[0]->name());
+
+	ASSERT_NE(nullptr, links[1]);
+	ASSERT_EQ("AREA", links[1]->name());
+}
