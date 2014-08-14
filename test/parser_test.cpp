02bc7c78cdb2b5aeeb09f4f5655f3e58a457c827
Povilas Balciunas <balciunas90@gmail.com>
2014-08-14 20:59:25 +0300

    Adds more parser tests.

--- test/parser_test.cpp
+++ test/parser_test.cpp
@@ -27,3 +27,37 @@ TEST(parser, advance_doctype_group)
 	auto doctype_end = html::parser::advance_doctype_group(html, '>');
 	ASSERT_EQ('<', doctype_end[0]);
 }
+
+
+TEST(parser, parse_exclamation_comment)
+{
+	html::parser parser(html::parser::parse_comments);
+
+	html::char_type html[] = "<!--html comment-->";
+	auto comment_end = parser.parse_exclamation(html);
+	ASSERT_EQ('\0', comment_end[0]);
+
+	auto doc = parser.get_document();
+	ASSERT_NE(nullptr, doc);
+
+	auto comment = doc->first_child();
+	ASSERT_NE(nullptr, comment);
+	ASSERT_EQ("html comment", comment->value());
+}
+
+
+TEST(parser, parse_exclamation_doctype)
+{
+	html::parser parser(html::parser::parse_doctype);
+
+	html::char_type html[] = "<!DOCTYPE html>";
+	auto doctype_end = parser.parse_exclamation(html);
+	ASSERT_EQ('\0', doctype_end[0]);
+
+	auto doc = parser.get_document();
+	ASSERT_NE(nullptr, doc);
+
+	auto doctype = doc->first_child();
+	ASSERT_NE(nullptr, doctype);
+	ASSERT_EQ("html", doctype->value());
+}
