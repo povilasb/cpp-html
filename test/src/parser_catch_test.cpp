61cd5c1280c43e082091d6260b311356f89f570f
Povilas Balciunas <balciunas90@gmail.com>
2015-03-10 22:31:33 +0200

    Added more parser tests.

--- test/src/parser_catch_test.cpp
+++ test/src/parser_catch_test.cpp
@@ -12,9 +12,40 @@ SCENARIO("cpphtml parser creates DOM document from html string", "[parser]")
 	{
 		cpphtml::parser parser;
 
-		WHEN("void element tag name does not end with />")
+		WHEN("void element with attributes tag name ends with />")
+		{
+			auto doc = parser.parse("<br class=line /><div></div>");
+
+			THEN("valid html document is created")
+			{
+				REQUIRE(doc->first_child()->name() == "BR");
+			}
+		}
+
+		WHEN("void element with attributes tag name does not end with />")
 		{
 			std::string str_html{"<br class=line /<div></div>"};
+
+			THEN("parse_error is thrown")
+			{
+				REQUIRE_THROWS_AS(parser.parse(str_html),
+					cpphtml::parse_error);
+			}
+		}
+
+		WHEN("void element without attribues tag name ends with />")
+		{
+			auto doc = parser.parse("<br/><div></div>");
+
+			THEN("valid html DOM document is created")
+			{
+				REQUIRE(doc->first_child()->name() == "BR");
+			}
+		}
+
+		WHEN("void element without attribues tag name does not end with />")
+		{
+			std::string str_html{"<br/<div></div>"};
 
 			THEN("parse_error is thrown")
 			{
